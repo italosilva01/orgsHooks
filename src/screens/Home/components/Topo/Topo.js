e1f0460446b27c0fe73530ef3fae2a2a98d1ad0a
Francisco Italo <italo.silvan87@gmail.com>
2024-03-15 21:12:29 -0300

    main: primeira versão estável

---
+++ src/screens/Home/components/Topo/Topo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+function Todo() {
+  return (
+    <View>
+      <Text>"ITALO TODO"</Text>;
+    </View>
+  );
+}
+
+export default Todo;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Todo from './components/Todo';
+function Home() {
+  return <Todo />;
+}
+export default Home;
